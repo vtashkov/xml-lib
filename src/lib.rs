237ed6ddd7945fd5a0e7124c173d218a73105ba9
Victor Tashkov <vtashkov@gmail.com>
2023-02-12 13:20:04 +0200

    adding not-so-dummy test

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn dummy_test() {
-        assert_eq!(1, 1);
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
     }
 }
